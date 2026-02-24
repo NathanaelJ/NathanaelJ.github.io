53a46801c58985e7a6149b469fb14e2fa1ad2bf0
Nathanael Jenkins <nathanaelaaronjenkins@gmail.com>
2026-02-24 10:59:42 -0500

    PhD candidate status
    Woohoo!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
         <img src="/resources-General/Self.jpg" class="introimg" alt="Nathanael Jenkins portrait">
         <div class="intro-text">
             <h1>Hi there!</h1>
-            <p>I'm Nathanael, a PhD student in Aeronautics and Astronautics at the <a href="https://www.mit.edu" target="_blank" rel="noreferrer noopener">Massachusetts Institute of Technology</a>. Welcome to my little space on the web.</p>
+            <p>I'm Nathanael, a PhD candidate in Aeronautics and Astronautics at the <a href="https://www.mit.edu" target="_blank" rel="noreferrer noopener">Massachusetts Institute of Technology</a>. Welcome to my little space on the web.</p>
         </div>
     </div>
 
